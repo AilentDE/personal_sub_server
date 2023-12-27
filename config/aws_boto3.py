782a4d405875ea6e9df5f4b8254eb3b731d1bdfc
D.E <cameronqoo@gmail.com>
2023-12-27 14:11:16 +0800

    upgrate bot3 setting

--- config/aws_boto3.py
+++ config/aws_boto3.py
@@ -11,6 +11,7 @@ def boto3Client(service:str='s3'):
         service,
         aws_access_key_id=setting.aws_access_key_id,
         aws_secret_access_key=setting.aws_secret_access_key,
+        aws_session_token = setting.aws_session_token,
         region_name=setting.region_name
     )
     try:
--- schema/settings.py
+++ schema/settings.py
@@ -6,8 +6,9 @@ class Settings(BaseSettings):
     smtp_from: str = os.getenv('SMTP_FROM')
     smtp_code: str = os.getenv('SMTP_CODE')
     mssql_url: str = os.getenv('MSSQL_URL')
-    aws_access_key_id: str = os.getenv('AWS_ACCESS_KEY_ID')
-    aws_secret_access_key: str = os.getenv('AWS_SECRET_ACCESS_KEY')
+    aws_access_key_id: str = os.getenv('AWS_ACCESS_KEY_ID') #lambda_default
+    aws_secret_access_key: str = os.getenv('AWS_SECRET_ACCESS_KEY') #lambda_default
+    aws_session_token: str = os.getenv('AWS_SESSION_TOKEN') #lambda_default
     region_name: str = os.getenv('REGION_NAME')
     s3_bucket_name: str = os.getenv('S3_BUCKET_NAME')
     ses_sender: str = os.getenv('SES_SENDER')
