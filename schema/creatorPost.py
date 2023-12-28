abce794640fa4f2d41d83428379a0ae100259597
D.E <cameronqoo@gmail.com>
2023-12-28 12:54:47 +0800

    modify creatorPost schema default

--- schema/creatorPost.py
+++ schema/creatorPost.py
@@ -4,24 +4,24 @@ from datetime import datetime
 
 class CreatorPostSchema(BaseModel):
     id: str
-    title: str|None
+    title: str|None = '新作品'
     layout: str
-    thumbnailAssetId: str|None
+    thumbnailAssetId: str|None = ''
     thumbnailDisplayMode: str
-    excerpt: str|None
-    bodyHtml: str|None
+    excerpt: str|None = ''
+    bodyHtml: str|None = ''
     visibility: str
-    visibleTierIds: List[str]|None
+    visibleTierIds: List[str]|None = []
     creatorField: str
-    albumAssetIds: List[str]|None
-    attachmentAssetsIds: List[str]|None
-    externalMediaUrl: str|None
-    projectId: str|None
+    albumAssetIds: List[str]|None = []
+    attachmentAssetsIds: List[str]|None = ''
+    externalMediaUrl: str|None = ''
+    projectId: str|None = ''
     isNsfw: bool
     isPinned: bool
-    tags: List[str]|None
-    publishedAt: datetime|None
-    projectTitle: str|None
+    tags: List[str]|None = []
+    publishedAt: datetime
+    projectTitle: str|None = ''
     creatorId: str|None = None
     isRecommend: bool = False
     viewsCount: int = 0
