dfe6103d2163d3b680fd408a1996738d5ba02251
D.E <cameronqoo@gmail.com>
2024-03-08 10:58:21 +0800

    enable oauth check

--- dependencies/oauth.py
+++ dependencies/oauth.py
@@ -5,14 +5,14 @@ from utils.jwt import decode_jwt
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/session/discord")
 
-# def oauth_check(token: Annotated[str, Depends(oauth2_scheme)]):
-#     return decode_jwt(token)
+def oauth_check(token: Annotated[str, Depends(oauth2_scheme)]):
+    return decode_jwt(token)
 
-def oauth_check(x_authorization: Annotated[str, Header(..., description='SwaggerUI僅支援完整Oauth2輸入，這個header作為暫時取代自動oauth驗證，忽略Bearer開頭。\n正式啟用時改回Oauth。')]):
-    scheme, param = x_authorization.split()
-    if scheme.lower() != 'bearer':
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail='Authorization ERROR.'
-        )
-    return decode_jwt(param)
+# def oauth_check(x_authorization: Annotated[str, Header(..., description='SwaggerUI僅支援完整Oauth2輸入，這個header作為暫時取代自動oauth驗證。\n正式啟用時改回Oauth。')]):
+#     scheme, param = x_authorization.split()
+#     if scheme.lower() != 'bearer':
+#         raise HTTPException(
+#             status_code=status.HTTP_403_FORBIDDEN,
+#             detail='Authorization ERROR.'
+#         )
+#     return decode_jwt(param)
